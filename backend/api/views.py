5c6d6c7e67132fc60fdf94beaf21ea40ea206176
Dulitha Daluwatta <dulitha.daluwatta@centric.eu>
2022-09-20 11:02:14 +0200

    model serializers introduced

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,18 +1,22 @@
-import json
 from django.forms.models import model_to_dict
 from django.http import JsonResponse
 
-from products.models import Product
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+from products.models import Product
+from products.serializers import ProductSerializer
+
+@api_view(["GET"])
 def api_home(request, *args, **kwarg):
-    model_data = Product.objects.all().order_by("?").first()
+    """
+    DRF API View
+    """
+
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-        # model instance (model_data)
-        # turn a python dict
-        # return JSON to my client
-    return JsonResponse(data)
+    if instance:
+        # data = model_to_dict(model_data, fields={
+        #                      'id', 'title', 'price', 'sale_price'})
+        data = ProductSerializer(instance).data 
+    return Response(data)
