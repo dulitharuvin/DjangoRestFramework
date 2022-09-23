0de96db678f6f4e4e8681e8f54662ce0720c6cd8
dulitharuvin <dulitha.ruvin@gmail.com>
2022-09-23 15:54:26 +0200

    viewsets implemented

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -3,11 +3,14 @@ from rest_framework import serializers
 
 from .models import Product
 
+
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Product
         fields = [
+            'pk',
             'title',
             'content',
             'price',
---
+++ backend/products/viewsets.py
@@ -0,0 +1,32 @@
+from rest_framework import mixins, viewsets
+
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    '''
+    get -> list -> Queryset
+    get -> retrieve -> Product instance detail view
+    post -> create -> New instance
+    put -> update
+    patch -> Partial update
+    delete -> destroy 
+    '''
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'  # default
+
+
+class ProductGenericViewSet(
+        mixins.ListModelMixin,
+        mixins.RetrieveModelMixin,
+        viewsets.GenericViewSet):
+    '''
+    get -> list -> Queryset
+    get -> retrieve -> Product instance detail view
+    '''
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'  # default
