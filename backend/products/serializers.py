c3d15b232eaa7f8c4696367e9a03279e6ed8b7a4
dulitharuvin <dulitha.ruvin@gmail.com>
2022-09-23 18:52:49 +0200

    serializer fields and create and update methods

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -11,10 +11,12 @@ class ProductSerializer(serializers.ModelSerializer):
         view_name='product-detail',
         lookup_field='pk'
     )
+    email = serializers.EmailField(write_only=True)
 
     class Meta:
         model = Product
         fields = [
+            'email',
             'url',
             'edit_url',
             'pk',
@@ -24,6 +26,16 @@ class ProductSerializer(serializers.ModelSerializer):
             'sale_price',
             'my_discount'
         ]
+
+    def create(self, validated_data):
+        email = validated_data.pop('email')
+        obj = super().create(validated_data)
+        # print(email, obj)
+        return obj
+
+    def update(self, instance, validated_data):
+        email = validated_data.pop('email')
+        return super().update(instance, validated_data)
 
     def get_edit_url(self, obj):
         # return f"/api/products/{obj.pk}/"
--- backend/products/views.py
+++ backend/products/views.py
@@ -13,7 +13,7 @@ class ProductListCreateAPIView(
 
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
-        print(serializer.validated_data)
+        # print(serializer.validated_data)
         title = serializer.validated_data.get('title')
         content = serializer.validated_data.get('content') or None
         if content is None:
