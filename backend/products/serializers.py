dbf45c77ed46646d34d16664a88aa09e962f75e2
dulitharuvin <dulitha.ruvin@gmail.com>
2022-09-23 23:08:44 +0200

    validtors in model serializers and seperate validators

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,7 +1,10 @@
 from cgitb import reset
+from turtle import title
+from wsgiref.validate import validator
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 from .models import Product
+from . import validators
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -11,30 +14,41 @@ class ProductSerializer(serializers.ModelSerializer):
         view_name='product-detail',
         lookup_field='pk'
     )
-    email = serializers.EmailField(write_only=True)
+    title = serializers.CharField(validators=[
+        validators.validate_title_no_hello,
+        validators.unique_product_title
+    ])
+    name = serializers.CharField(source='title', read_only=True)
 
     class Meta:
         model = Product
         fields = [
-            'email',
             'url',
             'edit_url',
             'pk',
             'title',
+            'name',
             'content',
             'price',
             'sale_price',
             'my_discount'
         ]
 
+    # def validate_title(self, value):
+    #     request = self.context.get('request)
+    #     user = request.user
+    #     qs = Product.objects.filter(title__iexact=value)
+    #     if qs.exists():
+    #         raise serializers.ValidationError(
+    #             f"{value} is already a product name.")
+    #     return value
+
     def create(self, validated_data):
-        email = validated_data.pop('email')
         obj = super().create(validated_data)
         # print(email, obj)
         return obj
 
     def update(self, instance, validated_data):
-        email = validated_data.pop('email')
         return super().update(instance, validated_data)
 
     def get_edit_url(self, obj):
